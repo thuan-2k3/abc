f796024e5fa63ba4fd62f2bb23a9ca4cdf84889d
thuan-2k3 <tnguyenminh2003@gmail.com>
2024-10-28 15:55:33 +0700

    abc

---
+++ Lab4-5/Controllers/CinemaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab4_5.Controllers
+{
+    public class CinemaController : Controller
+    {
+        // GET: Cinema
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListMovies()
+        {
+            string connectString = ConfigurationManager.ConnectionStrings["connectsql"].ConnectionString;
+            CinemaDBDataContext db  = new CinemaDBDataContext(connectString);
+            var movies = db.CinemaMovies.ToList();
+            return Json(movies, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Lab4-5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab4_5.MvcApplication" Language="C#" %>
